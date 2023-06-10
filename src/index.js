df4bef4f5a82a387416083de64bf259acd3f492e
Tigist Woldearegai <tigistlina@gmail.com>
2023-06-09 23:55:40 -0500

    changes background color and image with temp changes

--- src/index.js
+++ src/index.js
@@ -1,31 +1,49 @@
 const state = {
     currentTemp: 70,
+    backgroundColor: document.getElementById('buttom--left'),
+    landscapeImg: document.getElementById('buttom--right'),
 }
 
 // CHANGES TEMP
 const increaseTemp = () => {
     state.currentTemp++;
-    const tempControl = document.getElementById("temperature");
+    const tempControl = document.getElementById('temperature');
     tempControl.textContent = `${state.currentTemp}`
 };
-
-const registerIncreaseTempClick = () => {
-    const tempIncrease = document.getElementById("increase-t");
-    tempIncrease.addEventListener('click', increaseTemp)
-
-}
-document.addEventListener('DOMContentLoaded', registerIncreaseTempClick)
 
 const decreaseTemp = () => {
     state.currentTemp--;
-    const tempControl = document.getElementById("temperature");
+    const tempControl = document.getElementById('temperature');
     tempControl.textContent = `${state.currentTemp}`
 };
+// CHANGE BACKGROND COLOR  AND IMAGE WITH TEMP CHANGE
+const changeBackgrounds = () => {
+    if (state.currentTemp >= 80) {
+        state.backgroundColor.style.backgroundColor = 'red';
+        state.landscapeImg.style.backgroundImage = 'url(assets/landscape/cody-doherty-AeqlmVWtzFA-unsplash.jpg)';
 
-const registerDecreaseTemp = () => {
-    const tempIncrease = document.getElementById("decrease-t");
-    tempIncrease.addEventListener('click', decreaseTemp)
+    } else if (state.currentTemp >= 70) {
+        state.backgroundColor.style.backgroundColor = 'orange';
+        state.landscapeImg.style.backgroundImage = 'url(assets/landscape/alexandr-hovhannisyan-RkOtjbPuHZw-unsplash.jpg)';
+    } else if (state.currentTemp >= 60) {
+        state.backgroundColor.style.backgroundColor = 'yellow';
+        state.landscapeImg.style.backgroundImage = 'url(assets/landscape/andrew-neel-a_K7R1kugUE-unsplash.jpg)';
+    } else if (state.currentTemp >= 50) {
+        state.backgroundColor.style.backgroundColor = 'green';
+        state.landscapeImg.style.backgroundImage = 'url(assets/landscape/joel-jasmin-forestbird-4S3iMBIappo-unsplash.jpg)';
+    } else {
+        state.backgroundColor.style.backgroundColor = 'teal';
+    }
+}
+// REGISTER EVENT LISTENER
+const registerEventHandler = () => {
+    const tempDecrease = document.getElementById('decrease-t');
+    tempDecrease.addEventListener('click', decreaseTemp)
+    tempDecrease.addEventListener('click', changeBackgrounds)
 
+    const tempIncrease = document.getElementById('increase-t');
+    tempIncrease.addEventListener('click', increaseTemp)
+    tempIncrease.addEventListener('click', changeBackgrounds)
 }
 
-document.addEventListener('DOMContentLoaded', registerDecreaseTemp)
+document.addEventListener('DOMContentLoaded', registerEventHandler)
